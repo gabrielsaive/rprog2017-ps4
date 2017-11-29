7736f93c596595b765686669032a6cb4aadde519
gabrielsaive <gabrielsaive@github.com>
2017-11-29 18:20:58 +0100

    DC comics

--- gtrends-script.R
+++ gtrends-script.R
@@ -1,9 +1,10 @@
+install.packages("gtrendsR")
 library(gtrendsR)
 library(ggplot2)
 library(tidyverse)
+library(dplyr)
 
-
-bitcoin.trend <- gtrends(c("bitcoin"), gprop = "web", time = "all")[[1]]
+bitcoin.trend <- gtrends(c("Grenoble"), gprop = "web", time = "all")[[1]]
 class(bitcoin.trend)
 head(bitcoin.trend)
 
@@ -17,7 +18,7 @@ ggplot(data = bitcoin.trend) +
   geom_line(mapping = aes(x= date, y = hits)) +
   geom_vline(xintercept = as.Date("2017-01-20"), color = "red") #trump inauguration
   
-
+install.packages("Quandl")
 library(Quandl)
 bitcoin.price <- Quandl("BCHARTS/BITSTAMPUSD")
 bitcoin.price <- bitcoin.price %>% filter(Date %in% bitcoin.trend$date) %>% select(Date, Close) %>% rename(date = Date, price = Close) %>% mutate(price = price*100/max(price))
